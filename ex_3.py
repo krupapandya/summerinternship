4a5a7102ac27ca428872507d2b957af910a65c02
krupapandya <107407960+krupapandya@users.noreply.github.com>
2022-06-18 00:02:06 +0530

    Add files via upload

---
+++ ex_3.py
@@ -0,0 +1,9 @@
+n1 = 26
+print (n1, "is of type", type (n1))
+
+n2 = 28.8
+print (n2, "is of type", type (n2))
+print (n2, "is complex number?", isinstance(28.8,int))
+
+n3 = 1 + 2j
+print (n3, "is complex number?", isinstance (1 + 2j, complex))
